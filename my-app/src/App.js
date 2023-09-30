487f722f6b65c5bf87dfefabe9868556650dd453
uygsj <krutikhas@gmail.com>
2023-09-30 15:49:43 +0530

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import ProductList from './components/ProductList';
-
+import React from 'react';
+import ProductList from './Product/ProductList';
 
 function App() {
   return (
-    <ProductList/>
+    <div className="App">
+      <ProductList />
+    </div>
   );
 }
 
